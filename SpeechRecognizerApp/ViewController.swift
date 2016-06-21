db15a1c7b7500de36b175e25c7e686574030f0bf
Takayuki Sei <takayuki_sei@voyagegroup.com>
2016-06-21 21:47:44 +0900

    initial commit

---
+++ SpeechRecognizerApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SpeechRecognizerApp
+//
+//  Created by 清 貴幸 on 2016/06/21.
+//  Copyright © 2016年 VOYAGE GROUP, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
